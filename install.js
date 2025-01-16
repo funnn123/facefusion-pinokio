9db443e74216f71d4cfec758caf29cc04c2ad8cb
funnn123 <160131907+funnn123@users.noreply.github.com>
2025-01-16 11:40:23 +0700

    Update install.js

--- install.js
+++ install.js
@@ -33,7 +33,7 @@ module.exports = async kernel =>
 				method: 'shell.run',
 				params:
 				{
-					message: 'https://github.com/funnn123/facefusion-unlocked'
+					message: 'git clone https://github.com/funnn123/facefusion-unlocked --branch master --single-branch'
 				}
 			},
 			{
